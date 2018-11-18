27a681630f2cb83d34b78942dbf08d84c12d8ed4
Abhishek Dubey <abhishekdubey331@gmail.com>
2018-11-18 20:19:51 +0530

    init

---
+++ src/com/kanak/practice/AddServlet.java
@@ -0,0 +1,29 @@
+package com.kanak.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req,HttpServletResponse res)throws IOException,ServletException
+	{
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		int k=i+j;
+		
+		PrintWriter out=res.getWriter();
+		out.println(k);
+		/*req.setAttribute("k", k);
+		
+		RequestDispatcher rd=req.getRequestDispatcher("square");
+		rd.forward(req, res);*/
+		}	
+	}	
+
